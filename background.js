dbc8a8ed20a8f80c078d1376bd7f22e736c278ef
uschanna <107637137+uschanna@users.noreply.github.com>
2023-04-08 20:08:20 -0500

    ...

--- background.js
+++ background.js
@@ -1,12 +1,23 @@
-chrome.webRequest.onBeforeRequest.addListener(
-  function(details) {
-    if (details.url.includes("blocked-site.com")) {
-      return {cancel: true};
-    }
-  },
-  {urls: ["<all_urls>"]},
-  ["blocking"]
-);
+// Add a listener for the onInstalled event to register the rules
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [
+      {
+        id: "block_ads",
+        priority: 1,
+        action: {
+          type: "redirect",
+          redirect: {extensionPath: "/popup.html"}
+        },
+        condition: {
+          urlFilter: "*://*/*",
+          resourceTypes: ["script"]
+        }
+      }
+    ],
+    removeRuleIds: []
+  });
+});
 
 chrome.browserAction.onClicked.addListener(function() {
   chrome.browserAction.setPopup({popup: "popup.html"});
