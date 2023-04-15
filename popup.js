8cb2832d09c9a4aaa3771e03e4322bdce3a0d1c2
uschanna <107637137+uschanna@users.noreply.github.com>
2023-04-15 15:06:20 -0500

    test

--- popup.js
+++ popup.js
@@ -28,20 +28,14 @@ function startTimer() {
   decreaseButton.disabled = true;
   increaseButton.disabled = true;
 
-
   let minutes = parseInt(timer.textContent.split(':')[0]);
   let seconds = parseInt(timer.textContent.split(':')[1]);
   let totalSeconds = minutes * 60 + seconds;
 
-  timerInterval = setInterval(() => {
-    if (totalSeconds <= 0) {
-      clearInterval(timerInterval);
-      timerIsRunning = false;
-      startButton.disabled = false;
-      decreaseButton.disabled = false;
-      increaseButton.disabled = false;
+  // Send a message to the background script to start the timer
+  chrome.runtime.sendMessage({action: 'startTimer', totalSeconds});
 
-        // Listen for a message from the background script with the remaining time
+  // Listen for a message from the background script with the remaining time
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === 'updateTimer') {
       totalSeconds = message.remainingTime;
@@ -54,41 +48,38 @@ function startTimer() {
         startButton.disabled = false;
         decreaseButton.disabled = false;
         increaseButton.disabled = false;
-        
-      // Call the Cataas API to generate a random cat image
-      fetch('https://cataas.com/cat?json=true')
-        .then(response => response.json())
-        .then(data => {
-          const img = document.createElement('img');
-          img.src = `https://cataas.com${data.url}`;
-          img.classList.add('pixelated');
-          catContainer.appendChild(img);
 
-          // Save the image, time, and date to your-cafe.html
-          const card = document.createElement('div');
-          card.classList.add('card');
-          card.innerHTML = `
-            <img src="${img.src}" class="pixelated">
-            <p>Time: ${defaultTime} minutes</p>
-            <p>Date: ${new Date().toLocaleDateString()}</p>
-          `;
-          chrome.storage.local.get(['cards'], data => {
-            const cards = data.cards || [];
-            cards.push(card.outerHTML);
-            chrome.storage.local.set({cards});
+        // Call the Cataas API to generate a random cat image
+        fetch('https://cataas.com/cat?json=true')
+          .then(response => response.json())
+          .then(data => {
+            const img = document.createElement('img');
+            img.src = `https://cataas.com${data.url}`;
+            img.classList.add('pixelated');
+            catContainer.appendChild(img);
+
+            // Save the image, time, and date to your-cafe.html
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+              <img src="${img.src}" class="pixelated">
+              <p>Time: ${defaultTime} minutes</p>
+              <p>Date: ${new Date().toLocaleDateString()}</p>
+            `;
+            chrome.storage.local.get(['cards'], data => {
+              const cards = data.cards || [];
+              cards.push(card.outerHTML);
+              chrome.storage.local.set({cards});
+            });
+          })
+          .catch(error => {
+            console.log(error);
           });
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    } else {
-      totalSeconds--;
-      minutes = Math.floor(totalSeconds / 60);
-      seconds = totalSeconds % 60;
-      timer.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+      }
     }
-  }, 1000);
+  });
 }
+
 
 // Function to reset the timer
 function resetTimer() {
