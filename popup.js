5615da0602925950feacaff8f482a45f6ef6ee2e
uschanna <107637137+uschanna@users.noreply.github.com>
2023-04-17 20:24:34 -0500

    alarms

--- popup.js
+++ popup.js
@@ -1,115 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
-// Define variables
-const timer = document.getElementById('timer');
-  const startButton = document.getElementById('start-button');
-  const resetButton = document.getElementById('reset-button');
-  const decreaseButton = document.getElementById('decrease-button');
-  const increaseButton = document.getElementById('increase-button');
-
-const catContainer = document.getElementById('cat-container');
-let timerInterval;
+let totalSeconds = 1500;
 let timerIsRunning = false;
-let defaultTime = 25;
+let timerInterval;
 
-// Add event listeners
-startButton.addEventListener('click', startTimer);
-resetButton.addEventListener('click', resetTimer);
-decreaseButton.addEventListener('click', decreaseTime);
-increaseButton.addEventListener('click', increaseTime);
-
-// Set default time on page load
-timer.textContent = `${defaultTime.toString().padStart(2, '0')}:00`;
-
-// Function to start the timer
 function startTimer() {
-  timerIsRunning = true;
-  startButton.disabled = true;
-  resetButton.disabled = false;
-  decreaseButton.disabled = true;
-  increaseButton.disabled = true;
-  // change image to "cat-asleep.svg" when timer starts
-  const img = document.getElementById("imgClick");
-  img.src='assets/cat-asleep.svg'
-
-  let minutes = parseInt(timer.textContent.split(':')[0]);
-  let seconds = parseInt(timer.textContent.split(':')[1]);
-  let totalSeconds = minutes * 60 + seconds;
-
-  timerInterval = setInterval(() => {
-    if (totalSeconds <= 0) {
-      clearInterval(timerInterval);
+  if (!timerIsRunning) {
+    timerIsRunning = true;
+    chrome.alarms.create({delayInMinutes: totalSeconds/60}, function(alarm) {
       timerIsRunning = false;
-      startButton.disabled = false;
-      decreaseButton.disabled = false;
-      increaseButton.disabled = false;
-  // change image to "cat-awake.svg" when timer is done
-  img.src='assets/cat-awake.svg'
-
-      // Call the Cataas API to generate a random cat image
-      fetch('https://cataas.com/cat?json=true')
-        .then(response => response.json())
-        .then(data => {
-          const img = document.createElement('img');
-          img.src = `https://cataas.com${data.url}`;
-          img.classList.add('pixelated');
-          catContainer.appendChild(img);
-
-          // Save the image, time, and date to your-cafe.html
-          const card = document.createElement('div');
-          card.classList.add('card');
-          card.innerHTML = `
-            <img src="${img.src}" class="pixelated">
-            <p>Time: ${defaultTime} minutes</p>
-            <p>Date: ${new Date().toLocaleDateString()}</p>
-          `;
-          chrome.storage.local.get(['cards'], data => {
-            const cards = data.cards || [];
-            cards.push(card.outerHTML);
-            chrome.storage.local.set({cards});
-          });
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    } else {
+      clearInterval(timerInterval);
+      changeImage();
+    });
+    timerInterval = setInterval(() => {
       totalSeconds--;
-      minutes = Math.floor(totalSeconds / 60);
-      seconds = totalSeconds % 60;
-      timer.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    }
-  }, 1000);
+      updateTime();
+      if (totalSeconds <= 0) {
+        clearInterval(timerInterval);
+        timerIsRunning = false;
+        changeImage();
+      }
+    }, 1000);
+  }
 }
 
-// Function to reset the timer
 function resetTimer() {
-  const img = document.getElementById("imgClick");
-  img.src='assets/cat-awake.svg'
   clearInterval(timerInterval);
+  totalSeconds = 1500;
   timerIsRunning = false;
-  startButton.disabled = false;
-  resetButton.disabled = true;
-  decreaseButton.disabled = false;
-  increaseButton.disabled = false;
-  timer.textContent = `${defaultTime.toString().padStart(2, '0')}:00`;
-  catContainer.innerHTML = '';
+  updateTime();
+  document.getElementById("imgClick").src = "assets/cat-awake.svg";
 }
 
-// Function to decrease the time by 5 minutes
-function decreaseTime() {
-  defaultTime = Math.max(defaultTime - 5, 5);
-  timer.textContent = `${defaultTime.toString().padStart(2, '0')}:00`;
-}
-
-// Function to increase the time by 5 minutes
 function increaseTime() {
-  defaultTime += 5;
-  timer.textContent = `${defaultTime.toString().padStart(2, '0')}:00`;
+  totalSeconds += 300;
+  displayTime();
 }
 
-// Function to open the settings page
-function openSettings() {
-  // TODO: Implement the settings page
+function decreaseTime() {
+  if (totalSeconds >= 300) {
+    totalSeconds -= 300;
+    displayTime();
+  }
 }
+
+document.getElementById("start-button").addEventListener("click", startTimer);
+document.getElementById("reset-button").addEventListener("click", resetTimer);
+document.getElementById("increase-button").addEventListener("click", increaseTime);
+document.getElementById("decrease-button").addEventListener("click", decreaseTime);
+
   
   
 // add an event listener for "your-cafe-button" that opens your-cafe.html in a new tab
