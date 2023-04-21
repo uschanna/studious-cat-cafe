af6a109ddabb678c0038e4ecfa4a4292d9530059
uschanna <107637137+uschanna@users.noreply.github.com>
2023-04-20 21:13:57 -0500

    test alarms
    hm

--- background.js
+++ background.js
@@ -1,25 +1,44 @@
-let timerInterval;
-let remainingTime;
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'startTimer') {
-    remainingTime = message.totalSeconds;
-    timerInterval = setInterval(() => {
-      remainingTime--;
-      if (remainingTime <= 0) {
-        clearInterval(timerInterval);
-        // Send a message to the popup script with the remaining time
-        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-          chrome.tabs.sendMessage(tabs[0].id, {action: 'updateTimer', remainingTime});
-        });
-      }
-    }, 1000);
-  }
+chrome.alarms.create("pomodoroTimer", {
+  periodInMinutes: 1 / 60,
 });
 
-
-
-
-chrome.action.onClicked.addListener(function() {
-  chrome.action.setPopup({popup: "popup.html"});
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === "pomodoroTimer") {
+    chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
+      if (res.isRunning) {
+        let timer = res.timer + 1;
+        let isRunning = true;
+        if (timer > res.timeOption * 60) {
+          isRunning = false;
+          chrome.notifications.create({
+            type: "basic",
+            iconUrl: "assets/cat-awake.svg",
+            title: "Pomodoro Timer",
+            message: "The timer has ended!",
+          });
+          // Save the image, time, and date to chrome.storage.local
+          const date = new Date().toLocaleDateString();
+          const time = res.timeOption;
+          fetch(
+            `https://cataas.com/cat/says/timer%20iz%20done?width=300&height=200&size=50&json=true`
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              const img = `https://cataas.com${data.url}`;
+              const cards = res.cards || [];
+              cards.push({
+                img,
+                time,
+                date,
+              });
+              chrome.storage.local.set({ cards });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+        chrome.storage.local.set({ timer, isRunning });
+      }
+    });
+  }
 });
--- popup.js
+++ popup.js
@@ -23,66 +23,75 @@ document.addEventListener('DOMContentLoaded', () => {
   
   // Function to start the timer
   function startTimer() {
-    timerIsRunning = true;
-    startButton.disabled = true;
-    resetButton.disabled = false;
-    decreaseButton.disabled = true;
-    increaseButton.disabled = true;
-    // change image to "cat-asleep.svg" when timer starts
-    const img = document.getElementById("imgClick");
-    img.src='assets/cat-asleep.svg'
+    chrome.runtime.sendMessage({ action: "startPomodoroTimer", timeOption: defaultTime }, (response) => {
+      if (response.success) {
+        timerIsRunning = true;
+        startButton.disabled = true;
+        resetButton.disabled = false;
+        decreaseButton.disabled = true;
+        increaseButton.disabled = true;
+        // change image to "cat-asleep.svg" when timer starts
+        const img = document.getElementById("imgClick");
+        img.src = "assets/cat-asleep.svg";
   
-    let minutes = parseInt(timer.textContent.split(':')[0]);
-    let seconds = parseInt(timer.textContent.split(':')[1]);
-    let totalSeconds = minutes * 60 + seconds;
+        let minutes = parseInt(timer.textContent.split(":")[0]);
+        let seconds = parseInt(timer.textContent.split(":")[1]);
+        let totalSeconds = minutes * 60 + seconds;
   
-    timerInterval = setInterval(() => {
-      if (totalSeconds <= 0) {
-        clearInterval(timerInterval);
-        timerIsRunning = false;
-        startButton.disabled = false;
-        decreaseButton.disabled = false;
-        increaseButton.disabled = false;
-    // change image to "cat-awake.svg" when timer is done
-    img.src='assets/cat-awake.svg'
+        timerInterval = setInterval(() => {
+          if (totalSeconds <= 0) {
+            clearInterval(timerInterval);
+            timerIsRunning = false;
+            startButton.disabled = false;
+            decreaseButton.disabled = false;
+            increaseButton.disabled = false;
+            // change image to "cat-awake.svg" when timer is done
+            img.src = "assets/cat-awake.svg";
   
-        // Call the Cataas API to generate a random cat image
-          fetch('https://cataas.com/cat/says/timer%20iz%20done?width=300&height=200&size=50&json=true')
-          .then(response => response.json())
-          .then(data => {
-            const img = document.createElement('img');
-            img.src = `https://cataas.com${data.url}`;
-            img.classList.add('pixelated');
-            catContainer.appendChild(img);
-            timer.textContent = 'Done!'
-            // show "your-cafe-button" when timer is done
-            document.getElementById("your-cafe-button").style.display = "block";
-
-            // Save the image, time, and date to your-cafe.html
-            const card = document.createElement('div');
-            card.classList.add('card');
-            card.innerHTML = `
-              <img src="${img.src}" class="pixelated">
-              <p>Time: ${defaultTime} minutes</p>
-              <p>Date: ${new Date().toLocaleDateString()}</p>
-            `;
-            chrome.storage.local.get(['cards'], data => {
-              const cards = data.cards || [];
-              cards.push(card.outerHTML);
-              chrome.storage.local.set({cards});
-            });
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      } else {
-        totalSeconds--;
-        minutes = Math.floor(totalSeconds / 60);
-        seconds = totalSeconds % 60;
-        timer.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+            // Call the Cataas API to generate a random cat image
+            fetch(
+              "https://cataas.com/cat/says/timer%20iz%20done?width=300&height=200&size=50&json=true"
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                const img = document.createElement("img");
+                img.src = `https://cataas.com${data.url}`;
+                img.classList.add("pixelated");
+                catContainer.appendChild(img);
+                timer.textContent = "Done!";
+                // show "your-cafe-button" when timer is done
+                document.getElementById("your-cafe-button").style.display = "block";
+  
+                // Save the image, time, and date to your-cafe.html
+                const card = document.createElement("div");
+                card.classList.add("card");
+                card.innerHTML = `
+                <img src="${img.src}" class="pixelated">
+                <p>Time: ${defaultTime} minutes</p>
+                <p>Date: ${new Date().toLocaleDateString()}</p>
+              `;
+                chrome.storage.local.get(["cards"], (data) => {
+                  const cards = data.cards || [];
+                  cards.push(card.outerHTML);
+                  chrome.storage.local.set({ cards });
+                });
+              })
+              .catch((error) => {
+                console.log(error);
+              });
+          } else {
+            totalSeconds--;
+            minutes = Math.floor(totalSeconds / 60);
+            seconds = totalSeconds % 60;
+            timer.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
+              .toString()
+              .padStart(2, "0")}`;
+          }
+        }, 1000);
       }
-    }, 1000);
+    });
   }
+  
   
   // Function to reset the timer
   function resetTimer() {
