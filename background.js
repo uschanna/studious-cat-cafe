d62923f43f284ce4a86da18c17c8b327a1185950
uschanna <107637137+uschanna@users.noreply.github.com>
2023-04-21 23:38:34 -0500

    Basic logic is all finally working

--- background.js
+++ background.js
@@ -1,44 +1,46 @@
-chrome.alarms.create("pomodoroTimer", {
-  periodInMinutes: 1 / 60,
-});
+var intervalId;
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === "pomodoroTimer") {
-    chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
-      if (res.isRunning) {
-        let timer = res.timer + 1;
-        let isRunning = true;
-        if (timer > res.timeOption * 60) {
-          isRunning = false;
-          chrome.notifications.create({
-            type: "basic",
-            iconUrl: "assets/cat-awake.svg",
-            title: "Pomodoro Timer",
-            message: "The timer has ended!",
-          });
-          // Save the image, time, and date to chrome.storage.local
-          const date = new Date().toLocaleDateString();
-          const time = res.timeOption;
-          fetch(
-            `https://cataas.com/cat/says/timer%20iz%20done?width=300&height=200&size=50&json=true`
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              const img = `https://cataas.com${data.url}`;
-              const cards = res.cards || [];
-              cards.push({
-                img,
-                time,
-                date,
-              });
-              chrome.storage.local.set({ cards });
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        }
-        chrome.storage.local.set({ timer, isRunning });
-      }
-    });
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (!message.popupClosed) {
+    clearInterval(intervalId); // Stop your background function
   }
 });
+
+chrome.runtime.onConnect.addListener(function(port) {
+  if (port.name === "popup") {
+      port.onDisconnect.addListener(function() {
+        intervalId = setInterval(backgroundFunction, 1000); // Start your background function
+      });
+  }
+});
+
+function backgroundFunction() {
+  // Your background function code here
+  // if (alarm.name === "pomodoroTimer") {
+    chrome.storage.local.get(["timeSeconds", "timerIsRunning"], (res) => {
+      // If timer is not running, let's not do anything
+      if (!res.timerIsRunning) {
+        return;
+      }
+      let newTimeSeconds = Math.max(res.timeSeconds - 1, 0);
+      chrome.storage.local.set({ "timeSeconds": newTimeSeconds }, () => {
+        console.log("timeSeconds is set to " + newTimeSeconds);
+      });
+
+      // When we reach 0, let's clear the timer
+      if (newTimeSeconds === 0) {
+        // chrome.alarms.clear("pomodoroTimer");
+        chrome.notifications.create({
+          type: "basic",
+          iconUrl: "assets/cat-awake.svg",
+          title: "Pomodoro Timer",
+          message: "The timer has ended!",
+        });
+
+        chrome.storage.local.set({ "timerIsRunning": false }, () => {
+          console.log("timerIsRunning is set to " + false);
+        });
+      }
+    });
+  // }
+}
--- popup.js
+++ popup.js
@@ -8,20 +8,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const increaseButton = document.getElementById('increase-button');
   
   const catContainer = document.getElementById('cat-container');
-  let timerInterval;
+  let intervalId;
   let timerIsRunning = false;
-  let defaultTime = 25;
+  let defaultMinutes = 25;
+  let defaultSeconds = defaultMinutes * 60;
+  
 
-  // TODO: on start we want to set timeSeconds, but after it is set,
-  // we don't want to set it again
-  chrome.storage.local.get(["timeSeconds"], (res) => {
+  chrome.storage.local.get(["timeSeconds", "timerIsRunning", "timerIsDone"], (res) => {
     if (res.timeSeconds === undefined) {
-      chrome.storage.local.set({ "timeSeconds": 25 * 60 }).then(() => {
-        console.log("Value is set to " + 25*60);
+      chrome.storage.local.set({ "timeSeconds": defaultSeconds }).then(() => {
+        console.log("Value is set to " + defaultSeconds);
+      });
+    }
+    if (res.timerIsRunning === undefined) {
+      chrome.storage.local.set({ "timerIsRunning": false }).then(() => {
+        console.log("timerIsRunning is set to " + false);
+      });
+    }
+    if (res.timerIsDone === undefined) {
+      chrome.storage.local.set({ "timerIsDone": false }).then(() => {
+        console.log("timerIsDone is set to " + false);
       });
     }
   });
-  
+
   // Add event listeners
   startButton.addEventListener('click', startTimer);
   resetButton.addEventListener('click', resetTimer);
@@ -37,74 +47,118 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  // Load different page if timer is done
+  timerIsDone();
+
+  // Call it once before so that the state is set immediately
+  timeDecrement();
+
   // timer.textContent = `${defaultTime.toString().padStart(2, '0')}:00`;
-  
-  // Function to start the timer
-  function startTimer() {
-    chrome.storage.local.get(["timer", "timeOption", "timerIsRunning"], (res) => {
-      timerIsRunning = true;
+  function timeDecrement() {
+    chrome.storage.local.get(["timeSeconds", "timerIsRunning", "timerIsDone"], (res) => {
+      let totalSeconds = res.timeSeconds;
+      if (!res.timerIsRunning || res.timerIsDone) {
+        return;
+      }
+
+      // None of these buttons should be clickable when timer is running
       startButton.disabled = true;
       resetButton.disabled = false;
       decreaseButton.disabled = true;
       increaseButton.disabled = true;
+
       // change image to "cat-asleep.svg" when timer starts
       const img = document.getElementById("imgClick");
       img.src = "assets/cat-asleep.svg";
 
-      let minutes = parseInt(timer.textContent.split(":")[0]);
-      let seconds = parseInt(timer.textContent.split(":")[1]);
-      let totalSeconds = minutes * 60 + seconds;
+      if (totalSeconds <= 0) {
+        clearInterval(intervalId);
+        timerIsRunning = false;
+        startButton.disabled = false;
+        decreaseButton.disabled = false;
+        increaseButton.disabled = false;
+        chrome.storage.local.set({ "timerIsDone": true }, () => {
+          console.log("timerIsDone is set to " + true);
+        });
+        timerIsDone();
+      } else {
+        totalSeconds = Math.max(totalSeconds - 1, 0);
+        minutes = Math.floor(totalSeconds / 60);
+        seconds = totalSeconds % 60;
+        timer.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
+          .toString()
+          .padStart(2, "0")}`;
+        
+        chrome.storage.local.set({ "timeSeconds": totalSeconds }, () => {
+          console.log("timeSeconds is set to " + totalSeconds);
+        });
+      }
 
-      timerInterval = setInterval(() => {
-        if (totalSeconds <= 0) {
-          clearInterval(timerInterval);
-          timerIsRunning = false;
-          startButton.disabled = false;
-          decreaseButton.disabled = false;
-          increaseButton.disabled = false;
-          // change image to "cat-awake.svg" when timer is done
-          img.src = "assets/cat-awake.svg";
+      chrome.storage.local.set({ "timerIsRunning": true }, () => {
+        console.log("timerIsRunning is set to " + true);
+      });
 
-          // Call the Cataas API to generate a random cat image
-          fetch(
-            "https://cataas.com/cat/says/timer%20iz%20done?width=300&height=200&size=50&json=true"
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              const img = document.createElement("img");
-              img.src = `https://cataas.com${data.url}`;
-              img.classList.add("pixelated");
-              catContainer.appendChild(img);
-              timer.textContent = "Done!";
-              // show "your-cafe-button" when timer is done
-              document.getElementById("your-cafe-button").style.display = "block";
+    });
+  }
 
-              // Save the image, time, and date to your-cafe.html
-              const card = document.createElement("div");
-              card.classList.add("card");
-              card.innerHTML = `
-              <img src="${img.src}" class="pixelated">
-              <p>Time: ${defaultTime} minutes</p>
-              <p>Date: ${new Date().toLocaleDateString()}</p>
-            `;
-              chrome.storage.local.get(["cards"], (data) => {
-                const cards = data.cards || [];
-                cards.push(card.outerHTML);
-                chrome.storage.local.set({ cards });
-              });
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        } else {
-          totalSeconds--;
-          minutes = Math.floor(totalSeconds / 60);
-          seconds = totalSeconds % 60;
-          timer.textContent = `${minutes.toString().padStart(2, "0")}:${seconds
-            .toString()
-            .padStart(2, "0")}`;
-        }
-      }, 1000);
+  intervalId = setInterval(timeDecrement, 1000);
+
+
+  function timerIsDone() {
+    // change image to "cat-awake.svg" when timer is done
+    chrome.storage.local.get(["timerIsDone"], (res) => {
+      if (!res.timerIsDone) {
+        return;
+      }
+    });
+
+    timer.textContent = "Done!";
+    const img = document.getElementById("imgClick");
+    img.src = "assets/cat-awake.svg";
+
+    // Call the Cataas API to generate a random cat image
+    fetch(
+      "https://cataas.com/cat/says/timer%20iz%20done?width=300&height=200&size=50&json=true"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const img = document.createElement("img");
+        img.src = `https://cataas.com${data.url}`;
+        img.classList.add("pixelated");
+        catContainer.appendChild(img);
+        
+        // show "your-cafe-button" when timer is done
+        document.getElementById("your-cafe-button").style.display = "block";
+
+        // Save the image, time, and date to your-cafe.html
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.innerHTML = `
+        <img src="${img.src}" class="pixelated">
+        <p>Time: ${defaultMinutes} minutes</p>
+        <p>Date: ${new Date().toLocaleDateString()}</p>
+      `;
+        chrome.storage.local.get(["cards"], (data) => {
+          const cards = data.cards || [];
+          cards.push(card.outerHTML);
+          chrome.storage.local.set({ cards });
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  // Function to start the timer
+  function startTimer() {
+    chrome.storage.local.get(["timer", "timeSeconds", "timerIsRunning"], (res) => {
+      timerIsRunning = true;
+
+      timeDecrement();
+      
+      chrome.storage.local.set({ "timerIsRunning": timerIsRunning }, () => {
+        console.log("timerIsRunning is set to " + timerIsRunning);
+      });
     });
   }
   
@@ -113,14 +167,22 @@ document.addEventListener('DOMContentLoaded', () => {
   function resetTimer() {
     const img = document.getElementById("imgClick");
     img.src='assets/cat-awake.svg'
-    clearInterval(timerInterval);
+    clearInterval(intervalId);
     timerIsRunning = false;
     startButton.disabled = false;
     resetButton.disabled = true;
     decreaseButton.disabled = false;
     increaseButton.disabled = false;
-    timer.textContent = `${defaultTime.toString().padStart(2, '0')}:00`;
     catContainer.innerHTML = '';
+    chrome.storage.local.set({ "timeSeconds": defaultSeconds, "timerIsRunning": false, "timerIsDone": false }, () => {
+      console.log("in resetTimer: timeSeconds is set to " + defaultSeconds);
+      console.log("in resetTimer: timerIsRunning is set to " + false);
+      console.log("in resetTimer: timerIsDone is set to " + false);
+    });
+
+    let displayMinutes = Math.floor(defaultSeconds / 60);
+    let displaySeconds = defaultSeconds % 60;
+    timer.textContent = `${displayMinutes.toString().padStart(2, '0')}:${displaySeconds.toString().padStart(2, '0')}`;
   }
   
   // Function to decrease the time by 1 minutes
@@ -162,6 +224,10 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   });
-  
-    
-  
+
+chrome.runtime.connect({ name: "popup" });
+
+window.addEventListener('resize', function() {
+  // Your code to handle the load event
+  chrome.runtime.sendMessage({ popupClosed: false });
+});
